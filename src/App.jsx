8d83c2e8639a1b7d70a055b2c3d9606e2c4b375a
Affaled <120531449+Affaled@users.noreply.github.com>
2023-04-20 09:34:53 -0300

    ajuste na exibição

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import "./assets/styles/App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Header />
     </div>
   );
 }
