9939fc1715b74cf3b95d5aa59c11d527f598916a
Affaled <120531449+Affaled@users.noreply.github.com>
2023-04-21 19:13:00 -0300

    Header feito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,24 @@
 import React from "react";
+import logo from "../assets/svg/headerLogo.svg";
 import "../assets/styles/Header.css";
 
 export default function Header() {
   return (
     <header className="header">
-      <nav className="navBar">
-        <ul className="listBar">
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Projects</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-      <div className="nameBar">
-        <h1>Lucas Dias</h1>
-        <img src="" alt="Logo" className="headerLogo" />
+      <div className="headerContainer">
+        <nav className="navBar">
+          <ul className="listBar">
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Projects</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+        <div className="nameBar">
+          <h1>Lucas Dias</h1>
+          <img src={logo} alt="Logo" className="headerLogo" />
+        </div>
       </div>
     </header>
   );
