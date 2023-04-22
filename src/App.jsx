4169ab067f7c1da88c63bcf8636efb5633c3db72
Affaled <120531449+Affaled@users.noreply.github.com>
2023-04-22 08:32:43 -0300

    Criação das rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
-import Home from "./pages/Home";
 import "./assets/styles/App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Outlet />
     </div>
   );
 }
