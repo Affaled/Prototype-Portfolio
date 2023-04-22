0c7d49e45b3455d0905c7cc77ec8d4dc3204817c
Affaled <120531449+Affaled@users.noreply.github.com>
2023-04-22 08:32:57 -0300

    Ajuste da barra de navegação

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../assets/svg/headerLogo.svg";
 import "../assets/styles/Header.css";
 
@@ -6,19 +7,33 @@ export default function Header() {
   return (
     <header className="header">
       <div className="headerContainer">
+        <div className="nameBar">
+          <Link to="/">
+            <img src={logo} alt="Logo" className="headerLogo" />
+          </Link>
+          <Link to="/">
+            <h1>Lucas Dias</h1>
+          </Link>
+        </div>
         <nav className="navBar">
           <ul className="listBar">
-            <li>Home</li>
-            <li>About</li>
-            <li>Skills</li>
-            <li>Projects</li>
-            <li>Contact</li>
+            <Link to="/">
+              <li>Home</li>
+            </Link>
+            <Link to="/">
+              <li>About</li>
+            </Link>
+            <Link to="/">
+              <li>Skills</li>
+            </Link>
+            <Link to="/">
+              <li>Projects</li>
+            </Link>
+            <Link to="/">
+              <li>Contact</li>
+            </Link>
           </ul>
         </nav>
-        <div className="nameBar">
-          <h1>Lucas Dias</h1>
-          <img src={logo} alt="Logo" className="headerLogo" />
-        </div>
       </div>
     </header>
   );
