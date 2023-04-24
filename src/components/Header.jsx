645eaa0ccefeffe80c190e18e8043758e843703d
Affaled <120531449+Affaled@users.noreply.github.com>
2023-04-24 11:17:25 -0300

    Ajuste do SVG e começo da criação responsividade

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/svg/headerLogo.svg";
+import menu from "../assets/svg/menuIcon.svg";
 import "../assets/styles/Header.css";
 
 export default function Header() {
   return (
     <header className="header">
-      <div className="headerContainer">
-        <div className="nameBar">
-          <Link to="/">
-            <img src={logo} alt="Logo" className="headerLogo" />
-          </Link>
-          <Link to="/">
-            <h1>Lucas Dias</h1>
-          </Link>
-        </div>
-        <nav className="navBar">
-          <ul className="listBar">
-            <Link to="/">
-              <li>Home</li>
-            </Link>
-            <Link to="/">
-              <li>About</li>
-            </Link>
-            <Link to="/">
-              <li>Skills</li>
-            </Link>
-            <Link to="/">
-              <li>Projects</li>
-            </Link>
-            <Link to="/">
-              <li>Contact</li>
-            </Link>
-          </ul>
-        </nav>
+      <div className="nameBar">
+        <Link to="/">
+          <img src={logo} alt="Logo" />
+        </Link>
+        <Link to="/">
+          <p>Lucas Dias</p>
+        </Link>
       </div>
+      <nav>
+        <button className="menuIcon">
+          <img src={menu} alt="Menu" />
+        </button>
+        <ul className="listBar">
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/">
+            <li>About</li>
+          </Link>
+          <Link to="/">
+            <li>Skills</li>
+          </Link>
+          <Link to="/">
+            <li>Projects</li>
+          </Link>
+          <Link to="/">
+            <li>Contact</li>
+          </Link>
+        </ul>
+      </nav>
     </header>
   );
 }
