703dd115a4b98986dc21e10c74698e36f084ed1a
Affaled <120531449+Affaled@users.noreply.github.com>
2023-04-27 23:57:24 -0300

    implementação da responsividade

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,18 +7,15 @@ import "../assets/styles/Header.css";
 export default function Header() {
   return (
     <header className="header">
-      <div className="nameBar">
-        <Link to="/">
-          <img src={logo} alt="Logo" />
-        </Link>
-        <Link to="/">
-          <p>Lucas Dias</p>
-        </Link>
-      </div>
-      <nav>
-        <button className="menuIcon">
-          <img src={menu} alt="Menu" />
-        </button>
+      <nav className="navBar">
+        <div className="nameBar">
+          <Link to="/">
+            <img src={logo} alt="Logo" />
+          </Link>
+          <Link to="/">
+            <p>Lucas Dias</p>
+          </Link>
+        </div>
         <ul className="listBar">
           <Link to="/">
             <li>Home</li>
@@ -36,7 +33,29 @@ export default function Header() {
             <li>Contact</li>
           </Link>
         </ul>
+        <button className="menuIcon">
+          <img src={menu} alt="Menu" />
+        </button>
       </nav>
+      <div className="mobileMenu">
+        <ul>
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/">
+            <li>About</li>
+          </Link>
+          <Link to="/">
+            <li>Skills</li>
+          </Link>
+          <Link to="/">
+            <li>Projects</li>
+          </Link>
+          <Link to="/">
+            <li>Contact</li>
+          </Link>
+        </ul>
+      </div>
     </header>
   );
 }
