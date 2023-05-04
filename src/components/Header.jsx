f49062361b556b1ceefc3b2dfe9a718753955707
Affaled <120531449+Affaled@users.noreply.github.com>
2023-05-04 20:08:34 -0300

    construção do cabeçalho

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../assets/styles/Header.css";
 
 export default function Header() {
-  return <div></div>;
+  return (
+    <header className="header">
+      <div className="container">
+        <nav>
+          <Link to="/" className="logo">
+            Affaled
+          </Link>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/skills">Skills</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
