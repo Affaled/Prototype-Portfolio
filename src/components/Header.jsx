ef6daf490c0a70b60970331288d532e97c9a138c
Affaled <120531449+Affaled@users.noreply.github.com>
2023-07-24 18:27:12 -0300

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import style from "../assets/styles/components/Header.module.css";
 
 export default function Header() {
   return (
     <header>
-      <nav className="nav-bar">
-        <Link to="/">Home</Link>
-        <Link to="/">About me</Link>
-        <Link to="/">Projects</Link>
-        <Link to="/">Contact</Link>
+      <h1 className={style.logo}>
+        <Link to="/">Lucas Dias</Link>
+      </h1>
+      <nav>
+        <ul className={style.navList}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/">About</Link>
+          </li>
+          <li>
+            <Link to="/">Projects</Link>
+          </li>
+          <li>
+            <Link to="/">Contact</Link>
+          </li>
+        </ul>
       </nav>
     </header>
   );
