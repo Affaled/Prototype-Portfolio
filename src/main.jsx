d2d6dc73c76084a366902b103b3c6196a6443d38
Affaled <120531449+Affaled@users.noreply.github.com>
2023-07-24 17:39:55 -0300

    alterações no estilo global

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import routes from "./Routes";
+import "./assets/styles/global/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
