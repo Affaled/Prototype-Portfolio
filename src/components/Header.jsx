70855f03bebb14d3f760a7d049cd5b020adde312
Affaled <120531449+Affaled@users.noreply.github.com>
2023-09-14 03:39:52 -0300

    recomeço

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,5 @@
 import React from "react";
 
 export default function Header() {
-  return (
-    <header className="header">
-      <h1 className="header__logo">Affaled</h1>
-      <div className="header__container">
-        <nav className="header__container--nav">
-          <ul className="header__container--navList">
-            <li>About</li>
-            <li>Projects</li>
-            <li>Contact</li>
-          </ul>
-        </nav>
-        <p className="header__container--email">affaledl@gmail.com</p>
-      </div>
-    </header>
-  );
+  return <header className="header">Header</header>;
 }
