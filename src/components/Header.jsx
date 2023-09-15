687e6b89cf51a10e9e68c2e60b6d144c7e063d24
Affaled <120531449+Affaled@users.noreply.github.com>
2023-09-15 09:06:42 -0300

    criação do conteúdo da header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,25 +3,36 @@ import React from "react";
 export default function Header() {
   return (
     <header className="header">
+      <div className="header__logo">
+        <h1>Affaled</h1>
+      </div>
       <nav className="header__nav">
-        <ul className="header__nav-list">
+        <ul>
           <li>
-            <a href="#">Home</a>
+            <a href="#">HOME</a>
           </li>
           <li>
-            <a href="#">About</a>
+            <a href="#">ABOUT</a>
           </li>
           <li>
-            <a href="#">Projects</a>
+            <a href="#">PROJECTS</a>
           </li>
           <li>
-            <a href="#">Skills</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
+            <a href="#">CONTACT</a>
           </li>
         </ul>
       </nav>
+      <div className="header__socials">
+        <a href="#">
+          <i className="fab fa-linkedin"></i>
+        </a>
+        <a href="#">
+          <i className="fab fa-github"></i>
+        </a>
+        <a href="#">
+          <i className="fab fa-instagram"></i>
+        </a>
+      </div>
     </header>
   );
 }
