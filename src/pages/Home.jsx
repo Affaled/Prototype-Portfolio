825f350414f89247972161bd0f8bbba78946a928
Affaled <120531449+Affaled@users.noreply.github.com>
2023-09-15 10:12:56 -0300

    criação banner e correção no position

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,12 @@ import React from "react";
 export default function Home() {
   return (
     <main className="home">
-      <section className="home__banner">Section -1</section>
+      <section className="home__banner">
+        <div className="home__banner-title">
+          <h1>Front-End Developer</h1>
+          <p>SCROLL DOWN</p>
+        </div>
+      </section>
     </main>
   );
 }
