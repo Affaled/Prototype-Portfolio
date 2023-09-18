efa28d1eb8e05cbdc3fc93cd07f5c1d89946d6ea
Affaled <120531449+Affaled@users.noreply.github.com>
2023-09-18 11:12:30 -0300

    não faço ideia pois me perdi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
       <nav className="header__nav">
         <ul className="header__nav-list">
           <li className="header__nav-listItem">
-            <a href="/home" title="Home">
+            <a href="/" title="Home">
               Home
             </a>
           </li>
---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <main className="about">
+      <h1 className="about__title">About me</h1>
+    </main>
+  );
+}
