7c0f5697578a6d5edb5699ce45c9d6edc0f023d9
Affaled <120531449+Affaled@users.noreply.github.com>
2023-09-18 13:38:22 -0300

    about, selection e correções

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,33 @@ export default function About() {
   return (
     <main className="about">
       <h1 className="about__title">About me</h1>
+      <section className="about__biography">
+        <img src="#" alt="Lucas Photo" />
+        <section className="about__biography-text">
+          <h2 className="about__biography-title">Front-end developer.</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+            qui laborum omnis nobis aliquam! Ipsam rem illo cupiditate? Rem
+            consectetur ipsam fugit et incidunt perspiciatis dolorum minima ut
+            iure eveniet.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis
+            esse ducimus soluta placeat inventore minus possimus deleniti, sunt
+            ratione quod consequuntur! Soluta, accusantium? Illo animi iure
+            velit commodi, corrupti atque.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+            soluta aliquid illo? Adipisci inventore autem illo dignissimos,
+            nostrum vel eaque animi exercitationem omnis suscipit dolor pariatur
+            asperiores, veniam cumque! Iusto? Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Quae quas corrupti autem officiis eos
+            laborum. Cum dignissimos aut impedit fugit aliquam ex, deserunt
+            veritatis alias autem, nostrum unde, quas dolor!
+          </p>
+        </section>
+      </section>
     </main>
   );
 }
