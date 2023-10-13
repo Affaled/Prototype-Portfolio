f3f68a3ad2d21c114cd7e16dfc17ac365f493ede
Affaled <120531449+Affaled@users.noreply.github.com>
2023-10-13 19:07:30 -0300

    correção no nome do componente

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import routes from "./Routes";
+import Routes from "./Routes";
 import "./assets/styles/global/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={routes} />
+    <RouterProvider router={Routes} />
   </React.StrictMode>
 );
