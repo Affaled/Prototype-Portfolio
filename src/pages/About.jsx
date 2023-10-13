54ab7eae1a8166bebc7d072e3dabddaf8413869f
Affaled <120531449+Affaled@users.noreply.github.com>
2023-10-13 18:40:30 -0300

    adição de parte do conteúdo about me

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,8 @@ import css3 from "../assets/svg/css3.svg";
 import js from "../assets/svg/js.svg";
 import scss from "../assets/svg/scss.svg";
 import git from "../assets/svg/git.svg";
+import reactjs from "../assets/svg/reactjs.svg";
+import angular from "../assets/svg/angular.svg";
 
 export default function About() {
   return (
@@ -14,25 +16,27 @@ export default function About() {
         <section className="about__personal-text">
           <h3>Front-end developer.</h3>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
-            qui laborum omnis nobis aliquam! Ipsam rem illo cupiditate? Rem
-            consectetur ipsam fugit et incidunt perspiciatis dolorum minima ut
-            iure eveniet.
+            My involvement with programming began in 2017 when I took my first
+            steps into the world of coding with Python 3. Although, at that
+            time, I only completed a small portion of a course and soon
+            abandoned it, my passion for software development was reignited in
+            2022.
           </p>
           <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis
-            esse ducimus soluta placeat inventore minus possimus deleniti, sunt
-            ratione quod consequuntur! Soluta, accusantium? Illo animi iure
-            velit commodi, corrupti atque.
+            It was in that year that I decided to fully immerse myself in the
+            programming universe by creating games using the Game Maker Studio
+            platform. Subsequently, in 2023, I expanded my knowledge to web
+            development, dedicating myself to studying HTML and CSS for website
+            creation.
           </p>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
-            soluta aliquid illo? Adipisci inventore autem illo dignissimos,
-            nostrum vel eaque animi exercitationem omnis suscipit dolor pariatur
-            asperiores, veniam cumque! Iusto? Lorem ipsum dolor sit amet
-            consectetur, adipisicing elit. Quae quas corrupti autem officiis eos
-            laborum. Cum dignissimos aut impedit fugit aliquam ex, deserunt
-            veritatis alias autem, nostrum unde, quas dolor!
+            Currently, I am engaged in the study and development of web
+            applications, with a special focus on front-end development. I have
+            been committed to learning technologies like React, Angular, and
+            JavaScript. Furthermore, I am broadening my horizons to explore
+            back-end development, delving into robust technologies like Java and
+            PHP to enhance my skills and be capable of creating complete and
+            integrated systems."
           </p>
         </section>
       </section>
@@ -68,6 +72,8 @@ export default function About() {
           <img src={css3} alt="CSS3" />
           <img src={scss} alt="SCSS" />
           <img src={js} alt="JavaScript" />
+          <img src={reactjs} alt="ReactJS" />
+          <img src={angular} alt="Angular" />
         </div>
       </section>
     </main>
