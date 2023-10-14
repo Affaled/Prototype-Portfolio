a1b6475ff21359455a41f765c8e0e51b319d092b
Affaled <120531449+Affaled@users.noreply.github.com>
2023-10-14 20:13:52 -0300

    separação e criação de componente botão ler mais

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import MoreButton from "./MoreButton";
 
 export default function ProjectItem() {
   return (
@@ -13,28 +14,7 @@ export default function ProjectItem() {
             voluptatem ab iusto nam, culpa esse eligendi ea sapiente? Eaque.
           </p>
         </div>
-        <div className="project__item-text-more">
-          Learn more
-          <svg
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-            <g
-              id="SVGRepo_tracerCarrier"
-              stroke-linecap="round"
-              stroke-linejoin="round"></g>
-            <g id="SVGRepo_iconCarrier">
-              {" "}
-              <path
-                d="M6 12H18M18 12L13 7M18 12L13 17"
-                stroke="#000000"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"></path>{" "}
-            </g>
-          </svg>
-        </div>
+        <MoreButton />
       </div>
     </div>
   );
