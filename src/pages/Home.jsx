273b9512e679edd3fd5d9a27f0c66e96f9058958
Affaled <120531449+Affaled@users.noreply.github.com>
2023-10-14 20:14:22 -0300

    ajustes no home e botão ler mais

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import AboutText from "../components/AboutText";
 import SkillsSVG from "../components/SkillsSVG";
+import MoreButton from "../components/MoreButton";
 
 export default function Home() {
   return (
@@ -12,14 +13,17 @@ export default function Home() {
         </div>
       </section>
       <section className="home__about">
-        <div>
+        <div className="home__about-text">
           <h3>About</h3>
           <p>{AboutText.personal.paragraph1}</p>
           <p>{AboutText.professional.paragraph3}</p>
         </div>
-        <div>
-          <h3>Skills</h3>
-          <SkillsSVG />
+        <div className="home__about-container">
+          <div className="home__about-skills">
+            <h3>Skills</h3>
+            <SkillsSVG />
+          </div>
+          <MoreButton />
         </div>
       </section>
       <section className="home__projects">Projects</section>
