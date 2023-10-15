20000cc346911f436618ea4f1f2b94d4e4b34b85
Affaled <120531449+Affaled@users.noreply.github.com>
2023-10-15 15:58:46 -0300

    segunda tentativa carregamento svg bug

--- src/components/svgs.js
+++ src/components/svgs.js
@@ -1,13 +1,20 @@
 import React from "react";
+import html5 from "../assets/svg/html5.svg";
+import css3 from "../assets/svg/css3.svg";
+import js from "../assets/svg/js.svg";
+import scss from "../assets/svg/scss.svg";
+import git from "../assets/svg/git.svg";
+import reactjs from "../assets/svg/reactjs.svg";
+import angular from "../assets/svg/angular.svg";
 
 const svgs = {
-  html5: import("../assets/svg/html5.svg"),
-  css3: import("../assets/svg/css3.svg"),
-  js: import("../assets/svg/js.svg"),
-  scss: import("../assets/svg/scss.svg"),
-  git: import("../assets/svg/git.svg"),
-  reactjs: import("../assets/svg/reactjs.svg"),
-  angular: import("../assets/svg/angular.svg"),
+  html5,
+  css3,
+  js,
+  scss,
+  git,
+  reactjs,
+  angular,
 };
 
 export default svgs;
