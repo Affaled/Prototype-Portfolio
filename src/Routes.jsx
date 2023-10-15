8cf8fdf2bc078ad15fe3b4663e38972cd88897ba
Affaled <120531449+Affaled@users.noreply.github.com>
2023-10-15 20:26:37 -0300

    problema resolvido

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { createRoutes, BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 
-const AppRoutes = createRoutes([
+const AppRoutes = createBrowserRouter([
   {
     path: "/",
     element: <App />,
@@ -26,10 +26,4 @@ const AppRoutes = createRoutes([
   },
 ]);
 
-export default function Routes() {
-  return (
-    <BrowserRouter>
-      <Routes>{AppRoutes}</Routes>
-    </BrowserRouter>
-  );
-}
+export default AppRoutes;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import AppRoutes from "./Routes";
 import "./assets/styles/global/index.css";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <AppRoutes />
-  </BrowserRouter>,
-  document.getElementById("root")
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={AppRoutes} />
+  </React.StrictMode>
 );
