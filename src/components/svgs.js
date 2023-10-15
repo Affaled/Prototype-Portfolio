75ac89452337b7a0d0b8e20fa2bdf90e02851869
Affaled <120531449+Affaled@users.noreply.github.com>
2023-10-15 15:56:18 -0300

    tentativa de correção no carregamento dos svgs

--- src/components/svgs.js
+++ src/components/svgs.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const svgs = {
-  html5: React.lazy(() => import("../assets/svg/html5.svg")),
-  css3: React.lazy(() => import("../assets/svg/css3.svg")),
-  js: React.lazy(() => import("../assets/svg/js.svg")),
-  scss: React.lazy(() => import("../assets/svg/scss.svg")),
-  git: React.lazy(() => import("../assets/svg/git.svg")),
-  reactjs: React.lazy(() => import("../assets/svg/reactjs.svg")),
-  angular: React.lazy(() => import("../assets/svg/angular.svg")),
+  html5: import("../assets/svg/html5.svg"),
+  css3: import("../assets/svg/css3.svg"),
+  js: import("../assets/svg/js.svg"),
+  scss: import("../assets/svg/scss.svg"),
+  git: import("../assets/svg/git.svg"),
+  reactjs: import("../assets/svg/reactjs.svg"),
+  angular: import("../assets/svg/angular.svg"),
 };
 
 export default svgs;
