95990a3c965bf868de9ccdbbdd42a326a7368c5d
Affaled <120531449+Affaled@users.noreply.github.com>
2023-10-16 16:03:08 -0300

    ajustes about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,21 +5,15 @@ import AboutText from "../data/AboutData";
 const About = React.memo(() => {
   return (
     <main className="about">
-      <h2 className="about__title">About me</h2>
+      <h2>About me</h2>
       <div className="about__professional">
         <img src="#" alt="Lucas Photo" />
         <section className="about__professional-text">
           <h3>Front-end developer.</h3>
-          <p>{AboutText.professional.paragraph1}</p>
-          <p>{AboutText.professional.paragraph2}</p>
-          <p>{AboutText.professional.paragraph3}</p>
         </section>
       </div>
       <section className="about__personal">
         <h3>Getting to know me.</h3>
-        <p>{AboutText.personal.paragraph1}</p>
-        <p>{AboutText.personal.paragraph2}</p>
-        <p>{AboutText.personal.paragraph3}</p>
       </section>
       <section className="about__skills">
         <h3>My skills.</h3>
