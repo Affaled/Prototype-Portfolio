d1465b80aae9eb6d9d72c29417a7b3ded4b11c84
Affaled <120531449+Affaled@users.noreply.github.com>
2023-10-18 11:08:16 -0300

    ajuste responsivo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,35 +4,37 @@ import { Link } from "react-router-dom";
 const Header = React.memo(() => {
   return (
     <header className="header">
-      <h1>
-        <Link to="/">Affaled</Link>
-      </h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/projects">Projects</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-      <div className="header__social">
-        <a href="#">
-          <i className="fab fa-linkedin"></i>
-        </a>
-        <a href="#">
-          <i className="fab fa-github"></i>
-        </a>
-        <a href="#">
-          <i className="fab fa-instagram"></i>
-        </a>
+      <div className="header__content">
+        <h1>
+          <Link to="/">Affaled</Link>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="header__content-social">
+          <a href="#">
+            <i className="fab fa-linkedin"></i>
+          </a>
+          <a href="#">
+            <i className="fab fa-github"></i>
+          </a>
+          <a href="#">
+            <i className="fab fa-instagram"></i>
+          </a>
+        </div>
       </div>
     </header>
   );
