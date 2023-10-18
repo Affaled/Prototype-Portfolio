49d01276fb430ad0a2c65bf35183feecbfcc78d7
Affaled <120531449+Affaled@users.noreply.github.com>
2023-10-18 15:47:48 -0300

    separação do banner em componente fora de home

---
+++ src/components/HomeBanner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function HomeBanner() {
+  return (
+    <section className="homeBanner">
+      <div className="homeBanner__title">
+        <h1>Front-end developer</h1>
+        <p>Scroll down</p>
+      </div>
+    </section>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,11 @@
 import React from "react";
+import HomeBanner from "../components/HomeBanner";
 import HomeAbout from "../components/HomeAbout";
 
 const Home = React.memo(() => {
   return (
     <main className="home">
-      <section className="home__banner">
-        <div className="home__banner-title">
-          <h1>Front-end developer</h1>
-          <p>Scroll down</p>
-        </div>
-      </section>
+      <HomeBanner />
       <HomeAbout />
       <section className="home__projects">Projects</section>
       <section className="home__contact">Contact</section>
