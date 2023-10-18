7541f7fd07e5228d1b8cdb68b4b4a14496797b6f
Affaled <120531449+Affaled@users.noreply.github.com>
2023-10-18 11:27:11 -0300

    mudança na importação do reactDOM

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import RouteApp from "./routes";
 import "./assets/styles/global/index.css";
