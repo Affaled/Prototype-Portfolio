fbbcf0fde8890fb37e0ec33da14de62483858d40
Affaled <120531449+Affaled@users.noreply.github.com>
2023-10-18 11:26:30 -0300

    eu não sei o que está dando de errado com essa pag

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
 import { RouterProvider } from "react-router-dom";
 import RouteApp from "./routes";
 import "./assets/styles/global/index.css";
